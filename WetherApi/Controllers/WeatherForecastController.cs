c6994bb810204737d61b5266cf479651d6edc096
QuangDat965 <92535440+QuangDat965@users.noreply.github.com>
2024-07-10 11:05:08 +0700

    Update WeatherForecastController.cs

--- WetherApi/Controllers/WeatherForecastController.cs
+++ WetherApi/Controllers/WeatherForecastController.cs
@@ -14,7 +14,7 @@ namespace WeatherApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "ABC"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
